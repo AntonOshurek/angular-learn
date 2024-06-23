258b252f1382d0e0610dcaf6e38c2f57cfebb7a7
Anton Oshurek <oshurekdev@proton.me>
2024-06-23 20:17:15 +0200

    add signals

--- first/src/app/components/user/user.component.ts
+++ first/src/app/components/user/user.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 //DATA
 import { DUMMY_USERS } from '../../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -12,21 +12,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
-
-  get imageAlt() {
-    return 'photo of person - ' + this.selectedUser.name;
-  }
-
-  get name() {
-    return this.selectedUser.name;
-  }
+  selectedUser = signal(DUMMY_USERS[randomIndex()]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  imageAlt = computed(() => this.selectedUser().name);
+  name = computed(() => this.selectedUser().name);
 
   onSelectUser(evt: Event) {
     console.log(evt.target);
+    this.selectedUser.set(DUMMY_USERS[randomIndex()]);
   }
 }
