f9a97394e2b1b963c61c14c9bd82455cd0d2ab93
Anton Oshurek <oshurekdev@proton.me>
2024-06-27 18:46:29 +0200

    add date pipe

--- first/src/app/components/tasks/task/task.component.ts
+++ first/src/app/components/tasks/task/task.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
 //TYPES
 import type { IDummyTask } from '../../../data/dummy-tasks';
 import { CardComponent } from '../../shared/card/card.component';
@@ -8,25 +9,11 @@ import { CardComponent } from '../../shared/card/card.component';
   standalone: true,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
 })
 export class TaskComponent {
   @Input({ required: true }) task: IDummyTask;
   @Output() complete = new EventEmitter<string>();
-
-  get transformedDateForView(): string {
-    if (this.task.dueDate) {
-      const dt = new Date(this.task.dueDate);
-      const options: Intl.DateTimeFormatOptions = {
-        dateStyle: 'medium',
-        timeStyle: 'short',
-        timeZone: 'UTC',
-      };
-      return Intl.DateTimeFormat('en-EN', options).format(dt);
-    } else {
-      return '';
-    }
-  }
 
   onCompleteTask() {
     this.complete.emit(this.task.id);
