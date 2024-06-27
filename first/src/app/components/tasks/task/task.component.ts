452b6d35a0ffa77bf6a94742996718fdfcd2e738
Anton Oshurek <oshurekdev@proton.me>
2024-06-27 18:37:36 +0200

    add car component as a wraper

--- first/src/app/components/tasks/task/task.component.ts
+++ first/src/app/components/tasks/task/task.component.ts
@@ -1,13 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 //TYPES
 import type { IDummyTask } from '../../../data/dummy-tasks';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
+  imports: [CardComponent],
 })
 export class TaskComponent {
   @Input({ required: true }) task: IDummyTask;
