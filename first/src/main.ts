38b09ffb9706d36ee2c0b35a0320b5cc4867385b
Anton Oshurek <oshurekdev@proton.me>
2024-06-28 21:29:02 +0200

    create app module

--- first/src/main.ts
+++ first/src/main.ts
@@ -1,7 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//MODULES
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+//using this aproach when we have appcomponent as a stand alone component
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+
+// bootstrapApplication(AppComponent, appConfig).catch((err) =>
+//   console.error(err)
+// );
