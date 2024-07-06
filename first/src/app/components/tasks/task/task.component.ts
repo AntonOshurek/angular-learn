09862c651e44b0b01ebb668529c9e5f6eb5eaac7
Anton Oshurek <oshurekdev@proton.me>
2024-07-06 19:00:35 +0200

    move full app to modules

--- first/src/app/components/tasks/task/task.component.ts
+++ first/src/app/components/tasks/task/task.component.ts
@@ -1,18 +1,14 @@
 import { Component, inject, Input } from '@angular/core';
-import { DatePipe } from '@angular/common';
 //SERVICES
 import { TasksService } from '../tasks.service';
-//COMPONENTS
-import { CardComponent } from '../../shared/card/card.component';
 //TYPES
 import type { IDummyTask } from '../../../data/dummy-tasks';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
+  standalone: false,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
-  imports: [CardComponent, DatePipe],
 })
 export class TaskComponent {
   @Input({ required: true }) task: IDummyTask;
