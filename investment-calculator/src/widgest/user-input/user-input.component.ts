5f6b12b0293952994cd1d3a6626447d2404129bd
Anton <oshurekdev@proton.me>
2024-10-28 23:03:38 +0100

    add two way binding for form

--- investment-calculator/src/widgest/user-input/user-input.component.ts
+++ investment-calculator/src/widgest/user-input/user-input.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+//TYPES
+import { SubmittedData } from './user-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.scss'
+  styleUrl: './user-input.component.scss',
 })
 export class UserInputComponent {
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestmens = signal('0');
+  enteredExpectedReturn = signal('0');
+  enteredDuration = signal('0');
 
+  onSubmit() {
+    const submittedData: SubmittedData = {
+      initialInvestment: this.enteredInitialInvestment(),
+      annualInvestment: this.enteredAnnualInvestmens(),
+      expectedReturn: this.enteredExpectedReturn(),
+      duration: this.enteredDuration(),
+    };
+
+    console.log(submittedData);
+  }
 }
---
+++ investment-calculator/src/widgest/user-input/user-input.model.ts
@@ -0,0 +1,6 @@
+export interface SubmittedData {
+  initialInvestment: string;
+  annualInvestment: string;
+  expectedReturn: string;
+  duration: string;
+}
