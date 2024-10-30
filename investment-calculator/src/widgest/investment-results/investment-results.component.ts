23087417103a5a452b5274f6cd39be69f43de746
Anton <oshurekdev@proton.me>
2024-10-30 20:40:50 +0100

    add currency pipe for investment-result

--- investment-calculator/src/widgest/investment-results/investment-results.component.ts
+++ investment-calculator/src/widgest/investment-results/investment-results.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 //MODELS
 import type { AnnualData } from '../user-input/user-input.model';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.scss',
 })
