a4c390676f4b75867c0ce42c6dd36bfdcb742cb9
Anton Oshurek <oshurekdev@proton.me>
2024-11-15 15:49:53 +0100

    add host selector to styles and remove unnecesary elements from html

--- cmp-deep-dive/cmpdeepdive/src/app/shared/control/control.component.ts
+++ cmp-deep-dive/cmpdeepdive/src/app/shared/control/control.component.ts
@@ -7,6 +7,9 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.scss',
   encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+  },
 })
 export class ControlComponent {
   labelText = input.required<string>();
