de7d2131c453e3ef989e25ee8cca573e21d711e0
Anton Oshurek <oshurekdev@proton.me>
2024-11-15 16:23:29 +0100

    change servers status dinamicly

--- cmp-deep-dive/cmpdeepdive/src/app/dashboard/server-status/server-status.component.ts
+++ cmp-deep-dive/cmpdeepdive/src/app/dashboard/server-status/server-status.component.ts
@@ -8,5 +8,19 @@ import { Component, signal } from '@angular/core';
   styleUrl: './server-status.component.scss',
 })
 export class ServerStatusComponent {
-  currentStatus = signal<string>('online');
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus.set('online');
+      } else if (rnd < 0.9) {
+        this.currentStatus.set('offline');
+      } else {
+        this.currentStatus.set('unknown');
+      }
+    }, 5000);
+  }
 }
